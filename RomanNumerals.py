f5e794f8ec71392e8fa0bfaaddf20b0038189c2c
nrnb <nrnbenn@gmail.com>
2023-09-01 10:44:41 +0100

    Added Choose Mode in MAIN

---
+++ RomanNumerals.py
@@ -0,0 +1,12 @@
+def NumeralsToDecimal(numerals):
+    pass
+
+#Main
+print("Hello")
+if input("If you would like to do Roman Numerals to Decimal, Input '1'.") == "1":
+    print("You are in Roman Numerals To Decimal Mode")
+    print("")
+    NumeralsToDecimal(input("Please input your numerals that you would like to translate"))
+
+else:
+    print("That is not an option")
